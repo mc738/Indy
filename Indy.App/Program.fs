fff460359d4de136eefe38322f74e048847a8fac
max <me@mclifford.dev>
2021-08-04 21:48:11 +0100

    Initial commit

---
+++ Indy.App/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Indy.Core
+
+[<EntryPoint>]
+let main argv =
+    
+    let path = "/home/max/Data/_dummy"
+    let output = sprintf "/home/max/Data/_dummy_%s.artifact" (DateTime.Now.ToString("yyyyMMddHHmmss"))
+    let extractPath = "/home/max/Data/_dummy_clone"
+    
+    let archive = Archive.Create(path, output)
+    
+    archive.Extract extractPath
+    
+    0 // return an integer exit code
